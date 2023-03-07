59c4909f2d259cceac73875155316ed51eaa0412
Dev Agnihotri <86882295+DevAgnihotri@users.noreply.github.com>
2023-03-07 19:39:59 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,17 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "devdevagnihotri8@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: agnihotrivibhasanjeevdev108@gmail.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-    header("Location:thankyou.html");
+<<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$message = $_POST["message"];
+
+	$to = "devdevagnihotri8@gmail.com";
+	$subject = "New message from $name";
+	$body = "Name: $name\nEmail: $email\nMessage: $message";
+
+	if (mail($to, $subject, $body)) {
+		echo "Thank you for contacting us!";
+	} else {
+		echo "There was a problem sending your message.";
+	}
 }
-//redirect
 ?>
